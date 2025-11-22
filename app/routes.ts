3dee66ce988232ee1d888c132cd8f716d8589483
waseem abbas <waseemofficee@gamil.com>
2025-11-22 18:36:02 +0500

    `Removed progress and render routes and files`

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("./home.tsx"),
   route("/test", "./test.tsx"),
-  route("/api/lambda/progress", "./progress.tsx"),
-  route("/api/lambda/render", "./render.tsx"),
+  route("/api/lambda/progress", "./api/progress.tsx"),
+  route("/api/lambda/render", "./api/render.tsx"),
 ] satisfies RouteConfig;
